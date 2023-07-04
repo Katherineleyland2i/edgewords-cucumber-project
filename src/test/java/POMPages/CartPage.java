35838a5e55051f0678a687d4d76e646f1a733189
KatherineLeyland <katherine.leyland@2itesting.com>
2023-07-04 13:52:05 +0100

    Tidied up again

--- src/test/java/POMPages/CartPage.java
+++ src/test/java/POMPages/CartPage.java
@@ -47,19 +47,17 @@ public class CartPage {
     public int checkSubTotal() {
         String subTotal = driver.findElement(getSubTotal).getText();
         String cleanSubTotal = subTotal.replaceAll("[^0-9]", "");
-        int subTotalInt = Integer.parseInt(cleanSubTotal);
-        return subTotalInt;
+        return Integer.parseInt(cleanSubTotal);
     }
 
     public int checkTotal() {
         String total = driver.findElement(getTotal).getText();
         String cleanTotal = total.replaceAll("[^0-9]", "");
-        int totalInt = Integer.parseInt(cleanTotal);
-        return totalInt;
+        return Integer.parseInt(cleanTotal);
     }
 
 
-    public CheckoutPage getCheckoutPage() {
+    public void getCheckoutPage() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,500)", "");
         WebElement checkout = driver.findElement(linkText("Proceed to checkout"));
@@ -72,7 +70,6 @@ public class CartPage {
         WebDriverWait myWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         myWait.until(ExpectedConditions.elementToBeClickable(proceedToCheckoutButton));
         driver.findElement(proceedToCheckoutButton).click();
-        return new CheckoutPage(driver);
     }
 }
 
